eb10f01a18676058511bfc711403fae04c3e8068
Israel <isekundayo700@gmail.com>
2025-11-28 12:46:49 +0100

    use axios to fetch data

--- ecommerce-frontend/src/pages/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage.jsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import Header from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
 const HomePage = () => {
+  // fetch("http://localhost:3000/api/products")
+  // .then((response) => {
+  //   return response.json()
+  // }).then((data) => {
+  //   console.log(data);
+    
+  // })
+
+  axios.get("http://localhost:3000/api/products")
+  .then((response) => {
+    console.log(response.data);
+  })
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
