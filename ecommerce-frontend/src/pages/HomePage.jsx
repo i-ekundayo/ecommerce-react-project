e6080931505d30749facf7c85ed97f45d2cf1304
Israel <isekundayo700@gmail.com>
2025-11-28 13:35:32 +0100

    add server proxy config for /api and /images

--- ecommerce-frontend/src/pages/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage.jsx
@@ -16,12 +16,12 @@ const HomePage = () => {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products")
+    axios.get("/api/products")
       .then((response) => {
       setProducts(response.data);
     })
 
-    axios.get("http://localhost:3000/api/cart-items")
+    axios.get("/api/cart-items")
       .then((response) => {
         setCart(response.data);
       });
